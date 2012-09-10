4dcf46dc5d8ee295901eb146879d4967b671719b
Lecky Lao <leckylao@gmail.com>
2012-09-10 17:12:19 +1000

    modifiied setting for key and scret

--- lib/weibo.rb
+++ lib/weibo.rb
@@ -60,7 +60,9 @@ if File.exists?('config/weibo.yml')
   weibo_oauth = YAML.load_file('config/weibo.yml')[Rails.env || env || 'development']
   Weibo::Config.api_key = weibo_oauth["api_key"]
   Weibo::Config.api_secret = weibo_oauth["api_secret"]
-elsif ENV["API_KEY"] and ENV["API_SECRET"]
+elsif ENV["WEIBO_APP_ID"] and ENV["WEIBO_SECRET"]
+  Weibo::Config.api_key = ENV["WEIBO_APP_ID"]
+  Weibo::Config.api_secret = ENV["WEIBO_SECRET"]
 else 
   puts "\n\n=========================================================\n\n" +
        "  You haven't made a config/weibo.yml file.\n\n  You should.  \n\n  The weibo gem will work much better if you do\n\n" + 
